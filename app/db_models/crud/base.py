f0bd6fbbb04622b3a3d6296b04d2c270af46e625
Joshua Cole <9787792+jcole356@users.noreply.github.com>
2024-10-18 13:14:27 -0700

    Move projects crud operations to a separate file

--- app/db_models/crud/base.py
+++ app/db_models/crud/base.py
@@ -58,26 +58,6 @@ class BaseCRUD(CRUDInterface):
         self.db.commit()
 
 
-class ProjectCRUD(BaseCRUD):
-    def __init__(self, db: Session):
-        super().__init__(db, Project)
-    
-    def create(self, name: str, description: str):
-        return super().create(name=name, description=description)
-    
-    def get(self, id: int):
-        return super().get(id)
-    
-    def get_all(self):
-        return super().get_all()
-    
-    def update(self, id: int, name: str, description: str):
-        return super().update(id, name=name, description=description)
-    
-    def delete(self, id: int):
-        return super().delete(id)
-
-
 class TicketCRUD(BaseCRUD):
     def __init__(self, db: Session):
         super().__init__(db, Ticket)
