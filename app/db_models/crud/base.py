af45186de84f098cbbc0f5553f9f6d7a602fd8e8
Joshua Cole <9787792+jcole356@users.noreply.github.com>
2024-10-18 15:04:24 -0700

    Move kanban_status CRUD operations to a separate file

--- app/db_models/crud/base.py
+++ app/db_models/crud/base.py
@@ -56,23 +56,3 @@ class BaseCRUD(CRUDInterface):
         item = self.get(id)
         self.db.delete(item)
         self.db.commit()
-
-
-class KanbanStatusCRUD(BaseCRUD):
-    def __init__(self, db: Session):
-        super().__init__(db, KanbanStatus)
-    
-    def create(self, name: str, description: str, board_id: int):
-        return super().create(name=name, description=description, board_id=board_id)
-    
-    def get(self, id: int):
-        return super().get(id)
-    
-    def get_all(self):
-        return super().get_all()
-    
-    def update(self, id: int, name: str, description: str, board_id: int):
-        return super().update(id, name=name, description=description, board_id=board_id)
-    
-    def delete(self, id: int):
-        return super().delete(id)
---
+++ app/db_models/crud/kanban_status.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Session
+
+from app.db_models.kanban_status import KanbanStatus
+from app.db_models.crud.base import BaseCRUD
+
+class KanbanStatusCRUD(BaseCRUD):
+    def __init__(self, db: Session):
+        super().__init__(db, KanbanStatus)
+    
+    def create(self, name: str, description: str, board_id: int):
+        return super().create(name=name, description=description, board_id=board_id)
+    
+    def get(self, id: int):
+        return super().get(id)
+    
+    def get_all(self):
+        return super().get_all()
+    
+    def update(self, id: int, name: str, description: str, board_id: int):
+        return super().update(id, name=name, description=description, board_id=board_id)
+    
+    def delete(self, id: int):
+        return super().delete(id)
